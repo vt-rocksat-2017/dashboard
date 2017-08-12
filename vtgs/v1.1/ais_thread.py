5681d7033de4f99dcddfd8eae1a54312c0d2622a
Zach Leffke <zleffke@vt.edu>
2017-08-12 13:09:30 -0400

    adding vtgs version of dashboard script.

---
+++ vtgs/v1.1/ais_thread.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+#########################################
+#   Title: Rocksat Data Server Class    #
+# Project: Rocksat                      #
+# Version: 1.0                          #
+#    Date: August, 2017                 #
+#  Author: Zach Leffke, KJ4QLP          #
+# Comment: Initial Version              # 
+#########################################
+
+import socket
+import threading
+import sys
+import os
+import errno
+import time
+import binascii
+import numpy
+from Queue import Queue
+import datetime as dt
+from logger import *
+
+class AIS_Thread(threading.Thread):
+    def __init__ (self, options):
+        threading.Thread.__init__(self,name = 'AISThread')
+        self._stop          = threading.Event()
+        self.ip             = options.ais_ip
+        self.port           = options.ais_port
+        self.id             = options.id
+        self.ts             = options.ts
+        self.sock           = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP Socket
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.settimeout(1)
+        self.connected      = False
+        self.log_fh = setup_logger(self.id, 'ais_msg', self.ts)
+        self.logger = logging.getLogger('ais_msg')
+
+        self.last_frame_ts  = dt.datetime.utcnow()  #Time Stamp of last received frame
+        
+        self.ais_count = 0 #number of individual ADSB messages received
+        self.msgs = None
+        self.q = Queue()
+
+    def run(self):
+        print "AIS Thread Running..."
+        try:
+            print 'trying to connect to OpenCPN'
+            self.sock.connect((self.ip, self.port))
+            self.connected = True
+            print self.utc_ts() + "Connected to OpenCPN..."
+        except Exception as e:
+            self.Handle_Connection_Exception(e)
+
+        while (not self._stop.isSet()):
+            if (not self.q.empty()): #new message in Queue for downlink
+                ais_frame = self.q.get() #should be 256 byte messages
+                self.msgs = self.Decode_AIS_Frame(ais_frame)
+            if self.connected == True: 
+                self.send_to_plotter(self.msgs)
+            elif self.connected == False:
+                print self.utc_ts() + "Disconnected from OpenCPN..."
+                time.sleep(1)
+                try:
+                    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP Socket
+                    self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                    #self.sock.settimeout(1)
+                    self.sock.connect((self.ip, self.port))
+                    self.connected = True
+                    print self.utc_ts() + "Connected to OpenCPN..."
+                except Exception as e:
+                    self.Handle_Connection_Exception(e)
+
+    def Decode_AIS_Frame(self,frame):
+        data = frame[11:]
+        #print data
+        data_lines = data.split('!')
+        #print data_lines
+        msgs = []
+        for dl in data_lines:
+            if len(dl) > 0:
+                msgs.append("!" + dl)
+
+        #print msgs
+        for msg in msgs:
+            self.ais_count +=1
+            self.logger.info(str(self.ais_count)+','+msg)
+        return msgs
+
+    def send_to_plotter(self,msgs):
+        if msgs != None:
+            for msg in msgs:
+                try:
+                    self.sock.send(msg)
+                except Exception as e:
+                    self.Handle_Connection_Exception(e)
+        self.msgs = None
+
+    def Decode_ADSB_Frame(self, frame):
+        msgs = []
+        #print binascii.hexlify(frame)
+        #msg_type   = numpy.uint8(struct.unpack('>B',rx_frame[10]))[0]     #message type, 0=ADSB, 1=AIS, 2=HW
+        msg_cnt     = numpy.uint8(struct.unpack('>B',frame[11]))[0]         #Number of ADSB Messages
+        length      = numpy.uint32(struct.unpack('<I',frame[12:16]))[0]     #Length indicator field
+        data        = frame[16:]
+        #print msg_cnt, "{0:032b}".format(length), len(data)
+
+        MASK = numpy.uint32(1) #initialize 32 bit number to 31 0s and a 1
+        #print "{0:032b}".format(MASK) 
+        idx = 0 #first byte in data field index
+        for i in range(msg_cnt):
+            self.adsb_count += 1
+            msg_len = 0 #in bytes
+            if (MASK & length) > 0:  #long message
+                msg_len = 14
+                msg = data[idx:] 
+            else:  msg_len = 7
+            msg = data[idx:idx+msg_len]
+            idx = idx+msg_len
+            #print i, "{0:032b}".format(MASK), "{0:032b}".format(MASK & length), msg_len, binascii.hexlify(msg)
+            msgs.append(msg)
+            self.logger.info(str(self.adsb_count)+','+binascii.hexlify(msg))
+            MASK = MASK << 1 #bit shift MASK for next iteration
+        return msgs
+
+    def send_to_plotter_old(self,msgs):
+        for msg in msgs:
+            msg_str = '*' + str(binascii.hexlify(msg)) + ';\n'
+            #print 'sending', msg_str
+            try:
+                self.sock.send(msg_str)
+            except Exception as e:
+                self.Handle_Connection_Exception(e)
+            
+
+    def Handle_Connection_Exception(self, e):
+        #print e, type(e)
+        errorcode = e[0]
+        if errorcode==errno.ECONNREFUSED:
+            pass
+            #print errorcode, "Connection refused"
+        elif errorcode==errno.EISCONN:
+            print errorcode, "Transport endpoint is already connected"
+            self.sock.close()
+        else:
+            print e
+            self.sock.close()
+        self.connected = False
+
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.isSet()
+
+    def utc_ts(self):
+        return str(dt.datetime.utcnow()) + " UTC | "
+
---
+++ vtgs/v1.1/data_server.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+#########################################
+#   Title: Rocksat Data Server Class    #
+# Project: Rocksat                      #
+# Version: 1.0                          #
+#    Date: August, 2017                 #
+#  Author: Zach Leffke, KJ4QLP          #
+# Comment: Initial Version              # 
+#########################################
+
+import socket
+import threading
+import sys
+import os
+import errno
+import time
+import binascii
+import numpy
+
+import datetime as dt
+from logger import *
+
+class Data_Server(threading.Thread):
+    def __init__ (self, options):
+        threading.Thread.__init__(self,name = 'DataServer')
+        self._stop          = threading.Event()
+        self.ip             = options.ip
+        self.port           = options.port
+        self.id             = options.id
+        self.ts             = options.ts
+        self.sock           = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP Socket
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.connected      = False
+
+        self.log_fh = setup_logger(self.id, 'main', self.ts)
+        self.logger = logging.getLogger('main')
+
+        self.adsb_log_fh = setup_logger(self.id, 'adsb_frame', self.ts)
+        self.adsb_logger = logging.getLogger('adsb_frame')
+
+        self.ais_log_fh = setup_logger(self.id, 'ais_frame', self.ts)
+        self.ais_logger = logging.getLogger('ais_frame')
+
+
+        self.last_frame_ts  = dt.datetime.utcnow()  #Time Stamp of last received frame
+
+        self.frame_count        = 0
+        self.adsb_frame_count   = 0
+        self.ais_frame_count    = 0
+        self.hw_frame_count     = 0
+
+    def run(self):
+        print "Data Server Running..."
+        try:
+            self.sock.connect((self.ip, self.port))
+            self.connected = True
+            print self.utc_ts() + "Connected to Modem..."
+        except Exception as e:
+            self.Handle_Connection_Exception(e)
+
+        while (not self._stop.isSet()):
+            if self.connected == True: 
+                try:
+                    data = self.sock.recv(4096)
+                    if len(data) == 256:
+                        self.Decode_Frame(data, dt.datetime.utcnow())
+                    else:
+                        self.connected = False
+                except Exception as e:
+                    self.Handle_Connection_Exception(e)
+            elif self.connected == False:
+                print self.utc_ts() + "Disconnected from modem..."
+                time.sleep(1)
+                try:
+                    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP Socket
+                    self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                    self.sock.connect((self.ip, self.port))
+                    self.connected = True
+                    print self.utc_ts() + "Connected to Modem..."
+                except Exception as e:
+                    self.Handle_Connection_Exception(e)
+        sys.exit()
+
+    def Decode_Frame(self, rx_frame, ts):
+        self.frame_count += 1
+        self.last_frame_ts = ts
+        #print str(self.frame_count) + ',' + binascii.hexlify(rx_frame)
+        self.logger.info(str(self.frame_count) + ',' + binascii.hexlify(rx_frame))
+        self.Decode_Header(rx_frame)
+
+    def Decode_Header(self, rx_frame):
+        callsign    = str(rx_frame[0:6])     #Callsign
+        dn_pkt_id   = numpy.uint16(struct.unpack('>H',rx_frame[6:8]))[0]     #downlink frame id
+        up_pkt_id   = numpy.uint16(struct.unpack('>H',rx_frame[8:10]))[0]    #uplink frame id
+        msg_type    = numpy.uint8(struct.unpack('>B',rx_frame[10]))[0]     #message type, 0=ADSB, 1=AIS, 2=HW
+
+        msg_type_str = ""
+        if msg_type == 0: 
+            msg_type_str = 'ADSB'
+            self.adsb_frame_count += 1
+            self.adsb_cb.q.put(rx_frame)
+            self.adsb_logger.info(str(self.adsb_frame_count) + ',' + binascii.hexlify(rx_frame))
+        elif msg_type == 1: 
+            msg_type_str = ' AIS'
+            self.ais_frame_count += 1
+            #print rx_frame
+            self.ais_cb.q.put(rx_frame)
+            self.ais_logger.info(str(self.ais_frame_count) + ',' + binascii.hexlify(rx_frame))
+        elif msg_type == 2: msg_type_str = '  HW'
+        
+        print self.last_frame_ts, self.frame_count, callsign, dn_pkt_id, up_pkt_id, msg_type_str
+
+    def Handle_Connection_Exception(self, e):
+        #print e, type(e)
+        errorcode = e[0]
+        if errorcode==errno.ECONNREFUSED:
+            pass
+            #print errorcode, "Connection refused"
+        elif errorcode==errno.EISCONN:
+            print errorcode, "Transport endpoint is already connected"
+            self.sock.close()
+        else:
+            print e
+            self.sock.close()
+        self.connected = False
+
+    def set_adsb_callback(self, callback):
+        self.adsb_cb = callback
+        #print 'set adsb callback'
+
+    def set_ais_callback(self, callback):
+        self.ais_cb = callback
+        #print 'set adsb callback'
+
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.isSet()
+
+    def utc_ts(self):
+        return str(dt.datetime.utcnow()) + " UTC | "
+
